198e4a26e25d7d3dc0289ce0bc1cc4c048066c44
Gordon Sheen <gsheen@hawaii.edu>
2025-03-06 12:07:00 -1000

    finish Challenge-1

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -56,22 +56,18 @@ function Board({ xIsNext, squares, onPlay}) {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+  const re = /^(?:(?:...){0,2}([OX])\1\1|.{0,2}([OX])..\2..\2|([OX])...\3...\3|..([OX]).\4.\4)/;
+
+  // Replace null or empty squares with '-'
+  const boardString = squares.map((square) => (square ? square : "-")).join("");
+
+  const match = re.exec(boardString);
+
+  if (match) {
+    // One of the capturing groups 1, 2, 3, or 4 will hold the "X" or "O"
+    return match[1] || match[2] || match[3] || match[4];
   }
+
   return null;
 }
 
